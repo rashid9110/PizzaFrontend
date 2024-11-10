a5552cb4b0df66dfed8bf55ff4725f132306890f
MD Rashid Alam <alamr9253@gmail.com>
2024-11-10 14:17:03 +0530

    Added pizza image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import IconArrowRight from "../Components/Icon/Arrow_righr.jsx";
+import PizzaImage from "../assets/Images/pizza2.png"
 
 function Home() {
   return (
@@ -21,6 +22,14 @@ function Home() {
               <IconArrowRight/>
             </span>
            </button>
+        </div>
+        <div>
+        <img
+         src={PizzaImage}
+         alt="Pizza"
+         width={550}
+         height={550}
+        />
         </div>
       </section>
     </div>

2d7bf58c6af40e239c1e07265eb3c5d8c204d017
MD Rashid Alam <alamr9253@gmail.com>
2024-11-13 17:33:22 +0530

    Added first Slice as auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.js";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-    <App />
-    <Toaster/>
-  </BrowserRouter>,
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );

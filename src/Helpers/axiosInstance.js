40263c34d241d649d0f42f0adb64c4e81e42add3
MD Rashid Alam <alamr9253@gmail.com>
2025-02-17 20:24:59 +0530

    change

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-const axiosInstance =axios.create();//create a new instanceof axios
+// const axiosInstance =axios.create();//create a new instanceof axios
 
-axiosInstance.defaults.baseURL=import.meta.env.VITE_BACKEND_URL;//set the base URL
+// axiosInstance.defaults.baseURL=import.meta.env.VITE_BACKEND_URL;//set the base URL
 
-axiosInstance.defaults.withCredentials=true;//Allow cookies to be sent with the request
+// axiosInstance.defaults.withCredentials=true;//Allow cookies to be sent with the request
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+    withCredentials: true,  // Make sure this is set to send cookies with requests
+});
 
 export default axiosInstance;
